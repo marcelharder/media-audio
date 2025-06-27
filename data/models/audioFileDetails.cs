c49830656779e5354c7b197ecd3e09defaf37ded
m_harder88@yahoo.com <m_harder88@yahoo.com>
2025-06-27 21:33:30 +0200

    nog steeds bezig

--- data/models/audioFileDetails.cs
+++ data/models/audioFileDetails.cs
@@ -3,9 +3,9 @@ namespace data.models;
 public class AudioFileDetails
 {
     public int Id { get; set; }
+    public string? WorldIdentfier { get; set; }
     public string? AlbumTitle { get; set; }
     public string? CoverUrl { get; set; }
-
     public string? Artists { get; set; }
     public string[]? SongTitles { get; set; }
     public string[]? Allowed { get; set; }   
